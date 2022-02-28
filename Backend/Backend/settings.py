f448adfd65b9a171197faebd7ed1be46aa4e2552
Arik <BBArik@protonmail.com>
2022-02-28 00:14:44 -0500

    Update env name

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": os.getenv("DB_NAME"),
         "USER": os.getenv("DB_USER_ADMIN"),
-        "PASSWORD": os.getenv("DB_PASSWD"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
         "HOST": os.getenv("DB_HOST"),
         "PORT": os.getenv("DB_PORT"),
     }
