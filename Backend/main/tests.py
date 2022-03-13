264c3de0ac23530501938de212a29edad9820d46
Arik <BBArik@protonmail.com>
2022-03-13 00:42:55 -0500

    Add more tests templates and descriptions.

--- Backend/main/tests.py
+++ Backend/main/tests.py
@@ -43,16 +43,32 @@ class TestWorks(TestCase):
     def test_work_modification(self):
         """
         Test a work creation, deletion and modification
+
+        Steps:
+        1. Create a work
+        2. Modify random attributes
+        3. Remove a work that is reserved / lended
+
+        Test some with incorrect data and verify that the work will not be changed and will throw a warning as a result.
         """
 
     def test_work_checking(self):
         """
         Test to check if a work can be checked out and checked into a library
+
+        Steps (Use provided work):
+        1. Check out a work
+        2. Check in a work
         """
 
     def test_work_move_library(self):
         """
         Test to check if the work can be moved from one library to another
+
+        Steps (Use provided work and libraries):
+        1. Move a work from a library to another
+        2. Make sure that work is not in its original library and is in fact in the new library.
+        3. Make sure that the ID of the work updated successfully
         """
 
 
@@ -60,6 +76,13 @@ class TestUsers(TestCase):
     def test_user_modification(self):
         """
         Test creating, modifying and deleting a User
+
+        Steps:
+        1. Create a user
+        2. Modify a user data
+        3. Delete a user
+
+        Test some with incorrect data and verify that the user will not be changed and will throw a warning as a result.
         """
 
     def test_user_authentication(self):
@@ -67,16 +90,47 @@ class TestUsers(TestCase):
         Test user authentication
         For normal users
         For Admins
+
+        Steps (Do this for each type of user):
+        1. Authenticate with correct credentials
+        2. Verify if the user is logged in
+        3. Log out
+        4. Authenticate with invalid credentials
+        5. Verify if the user is logged in
+        6. Try to make a user action (reserve a work)
+        7. Try to log out
         """
 
     def test_user_promoting(self):
         """
         Test user promotion to a different state (Employee, Admin, etc)
+
+        Steps:
+        1. Create base User
+        2. Upgrade it for each level (Each level needs the user to have some requirements by default)
         """
 
     def test_user_check_work(self):
         """
         Test user access to different works. (Work can be in a 'staging' area where they are not accessible)
+
+        Steps (For every access right):
+        1. Access a work that is public
+        2. Access "restricted" work
+        """
+
+    def test_user_get_recommended(self):
+        """
+        Test getting recommended books.
+
+        Step:
+        1. Create new user
+        2. Check out the recommended works (should be random, different types)
+
+        Steps (For 3 random work types):
+        1. Check out 10 work of one type
+        2. Check out the recommended works (should of given types)
+        :return:
         """
 
 
@@ -84,9 +138,57 @@ class TestLibrary(TestCase):
     def test_library_modification(self):
         """
         Test Library creation, deletion and modification
+
+        Steps:
+        1. Create library
+        2. Modify library
+        3. Delete library
+
+        Test some with incorrect data and verify that the library will not be changed and will throw a warning as a result.
         """
 
     def test_library_add_work(self):
         """
         Test adding and removing works from a library
+
+        Steps:
+        1. Add a work
+        2. Remove work
+
+        Test some with incorrect data and verify that the library will not be changed and will throw a warning as a result.
+        """
+
+    def test_library_get_top_works(self):
+        """
+        Test getting top works from a library
+
+        Steps:
+        1. Create library
+        2. Check out the top works (should be random)
+
+        Steps:
+        1. Check out and check back in multiple random works multiple times
+        2. Check out the top works (should correspond to the works that have been checked out the most)
+        :return:
+        """
+
+
+class TestComment(TestCase):
+    def test_comment_add(self):
+        """
+        Test adding a comment on a work
+
+        Steps:
+        1. Add a normal comment
+        2. Add a comment with bad words (Should not be accepted)
+        3. Add a comment with html tags / script (Should not be accepted)
+        """
+
+    def test_command_remove(self):
+        """
+        Test removing a comment
+
+        Steps:
+        1. Add a normal comment
+        2. Remove it
         """
