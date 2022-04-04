6374873facd9a0effa432a70c00fa8c4eb7f1f0c
Arik <BBArik@protonmail.com>
2022-04-04 17:59:24 -0400

    edit admin

--- Backend/main/commands.py
+++ Backend/main/commands.py
@@ -51,62 +51,90 @@ def recherche_avance():
 def create_book(response, liste_info):
     if response.method == "POST":
 
-        for info in liste_info: #Vérifie que tout les éléments ont une longeur d'au moins 3.
-            if len(str(response.POST.get(str(info)))) < 3 and info != "price" and info != "numeroCopie" and info != "nbrPage":
-                return False # Si il y a une erreur retourne False
+        for (
+            info
+        ) in liste_info:  # Vérifie que tout les éléments ont une longeur d'au moins 3.
+            if (
+                len(str(response.POST.get(str(info)))) < 3
+                and info != "price"
+                and info != "numeroCopie"
+                and info != "nbrPage"
+            ):
+                return False  # Si il y a une erreur retourne False
 
-        try:
-            nom_livre = response.POST.get("nomLivre")
-            author_name = response.POST.get("authorName")
-            date_publication = response.POST.get("datePublication")
-            edition_house = response.POST.get("editionHouse")
-            nombre_page = response.POST.get("nbrPage")
-            resume = response.POST.get("resume")
-            genre = response.POST["dropdown_genre"]
-            language = response.POST.get("language")
-            etat = response.POST["dropdown_etat"]
-            numero_copie = response.POST.get("numeroCopie")
-            type_livre = response.POST["dropdown_type"]
-            price = response.POST.get("price")
+        print(response.POST["dropdown_genre"])
 
-            # modification de la date de publication:
-            date_publication_list = date_publication.split("-")
-            date_publication = datetime.date(
-                int(date_publication_list[0]),
-                int(date_publication_list[1]),
-                int(date_publication_list[2]),
-            )
+        for txt in liste_info:
+            print(txt + ": " + str(response.POST.get(txt)))
 
-            # Création de l'id (Pour l'instant j'ai fait des enum par défaut en attendant que je finisse le html avec les
-            # bandes déroulantes)
-            val_id = str(
-                generalIdCreationAndManagement(
-                    10, True, PermissionEnums.AA, genre, type_livre
-                )
+        """try:"""
+        nom_livre = response.POST.get("nomLivre")
+        author_name = response.POST.get("authorName")
+        date_publication = response.POST.get("datePublication")
+        edition_house = response.POST.get("editionHouse")
+        nombre_page = response.POST.get("nbrPage")
+        resume = response.POST.get("resume")
+        genre = response.POST["dropdown_genre"]
+        language = response.POST.get("language")
+
+        etat = response.POST["dropdown_etat"]
+
+        numero_copie = response.POST.get("numeroCopie")
+        type_livre = response.POST["dropdown_type"]
+        price = response.POST.get("price")
+
+        # modification de la date de publication:
+        date_publication_list = date_publication.split("-")
+        date_publication = datetime.date(
+            int(date_publication_list[0]),
+            int(date_publication_list[1]),
+            int(date_publication_list[2]),
+        )
+
+        # Création de l'id (Pour l'instant j'ai fait des enum par défaut en attendant que je finisse le html avec les
+        # bandes déroulantes)
+
+        val_id = str(
+            generalIdCreationAndManagement(
+                10, True, PermissionEnums.AA, genre, type_livre
             )
-            # Création de l'objet
-            livre = WorkList(
-                id_works=val_id,
-                name_works=str(nom_livre),
-                author_name=str(author_name),
-                publication_date=date_publication,
-                edition_house=str(edition_house),
-                length=int(nombre_page),
-                resume=str(resume),
-                genre=str(genre),
-                language=str(language),
-                state=int(etat),
-                copy_number=int(numero_copie),
-                type_work=str(type_livre),
-                price=decimal.Decimal(price),
-            )
-            livre.save()  # Enregistrement de l'objet
-            return True #Retourne True si il le livre est créé. 
-        except:
-            return False #Retroune False si le livre n'est pas créé. 
+        )
+        print("val id:" + val_id)
+
+        # Création de l'objet
+        livre = WorkList(
+            id_works=val_id,
+            name_works=str(nom_livre),
+            author_name=str(author_name),
+            publication_date=date_publication,
+            edition_house=str(edition_house),
+            length=int(nombre_page),
+            resume=str(resume),
+            genre=str(genre),
+            language=str(language),
+            state=int(etat),
+            copy_number=int(numero_copie),
+            type_work=str(type_livre),
+            price=decimal.Decimal(price),
+        )
+        livre.save()  # Enregistrement de l'objet
+        print("livre créé")
+        return True  # Retourne True si il le livre est créé.
+    """except:
+    print('erreur')
+    return False  # Retroune False si le livre n'est pas créé."""
+
+
+def search_book_by_id(response):
+    return WorkList.objects.all().filter(id_works=response.POST.get("id_work"))
 
 
 def supprimer_livre(response):
-    id_delete = response.POST.get("id_work") #Id du livre à delete
-    WorkList.objects.filter(id_works=str(id_delete)).delete() #Delete le livre
-            
+    id_delete = response.POST.get("id_work")  # Id du livre à delete
+    WorkList.objects.filter(id_works=str(id_delete)).delete()  # Delete le livre
+
+
+def edit_book_admin(response, liste_info):
+    WorkList.objects.filter(id_works=str(response.POST.get("id_edit"))).delete()
+    print(response.POST["dropdown_genre"])
+    create_book(response, liste_info)
